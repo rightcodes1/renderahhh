e0976829546149305750a65f1273ed9a057291c4
rightcodes1 <139355567+rightcodes1@users.noreply.github.com>
2026-08-01 05:11:06 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -4,10 +4,9 @@
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -o yt-dlp
 chmod +x yt-dlp
 
-# Download static ffmpeg (required for merging video+audio)
+# Download & extract static ffmpeg into ./bin/
+mkdir -p bin
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
-tar -xf ffmpeg.tar.xz
-mv ffmpeg-*-amd64-static/ffmpeg ffmpeg
-mv ffmpeg-*-amd64-static/ffprobe ffprobe
-chmod +x ffmpeg ffprobe
-rm -rf ffmpeg-*-amd64-static ffmpeg.tar.xz
+tar -xf ffmpeg.tar.xz -C bin/ --strip-components=1
+chmod +x bin/ffmpeg bin/ffprobe
+rm ffmpeg.tar.xz
