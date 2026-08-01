dcb6fc11a962c4ae444ebc5f6117acf876bf2a1b
rightcodes1 <139355567+rightcodes1@users.noreply.github.com>
2026-08-01 03:18:07 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,22 +1,19 @@
 const { ActivityType } = require("discord.js");
-const CONFIG = require("../config");
 
 module.exports = {
   name: "ready",
   once: true,
   execute(client, commands) {
     console.log(`Logged in as ${client.user.tag}!`);
-        // Register slash commands globally
-    const commandData = commands.map(command => command.data.toJSON());
+    
+    // FIX: Convert Collection to Array before mapping
+    const commandData = Array.from(commands.values()).map(command => command.data.toJSON());
+    
     client.application.commands.set(commandData)
       .then(() => console.log("Successfully registered application commands."))
       .catch(console.error);
 
     console.log(`Serving in ${client.guilds.cache.size} guilds`);
-    
-    // Set bot status
     client.user.setActivity("downloading TikToks", { type: ActivityType.Watching });
-    
-
   }
 };
