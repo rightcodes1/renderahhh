efd70839e10f55fa0d54587988e2a1e54f989bed
rightcodes1 <139355567+rightcodes1@users.noreply.github.com>
2026-08-01 04:48:35 +0300

    Add files via upload

---
+++ render-build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# download yt-dlp standalone binary (no python required)
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -o yt-dlp
+chmod +x yt-dlp
