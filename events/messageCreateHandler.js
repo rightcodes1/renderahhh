d06930d414d028ef3d59e40b1c3a83e161fa52b5
rightcodes1 <139355567+rightcodes1@users.noreply.github.com>
2026-08-01 04:25:18 +0300

    Update messageCreateHandler.js

--- events/messageCreateHandler.js
+++ events/messageCreateHandler.js
@@ -1,5 +1,6 @@
 const { Events, EmbedBuilder } = require("discord.js");
 const tiktok = require('tiktok-scraper-without-watermark');
+const result = await tiktok.tiklydown(tiktokURL)
 const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
